a9f3dd2563f4da702d96d8f251d7629595436c5d
unknown <kaniksharma@bitbucket.org>
2018-11-07 16:01:14 +1100

    test added

---
+++ src/main/java/com/accesshq/apitest/APITest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.accesshq.apitest;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.ValidatableResponse;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Kanik Sharma
+ */
+public class APITest 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Test");
+/*        System.out.println(URI);
+        ArrayList<String> res = RestAssured.
+                given().
+                when().get(URI).
+                then().extract().path("locations");
+  
+        System.out.println(res.size());
+        System.out.println(res);
+        int i=0;
+        do
+        {
+            tempStop = RestAssured.given().when().get(URI).then().extract().path("locations["+i+"].name");
+            System.out.println(tempStop);
+            ArrayList<Integer> mode = RestAssured.given().when().get(URI).then().extract().path("locations["+i+"].modes");
+            if(mode.size()>0)
+            {
+                System.out.println("Multiple modes of transportation found.");
+            }
+            else
+            {
+                
+            }
+            i++;
+        }
+        while(tempStop.equalsIgnoreCase(expectedStop));*/
+
+        
+        ////////////
+        
+        
+        
+    }
+}
